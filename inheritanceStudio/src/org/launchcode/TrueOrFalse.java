86c66ad26d9cdf46d3a8bf184a3a589b3524b9d7
forsythet@gmail.com <forsythet@gmail.com>
2023-09-14 19:13:54 -0500

    setup project

---
+++ inheritanceStudio/src/org/launchcode/TrueOrFalse.java
@@ -0,0 +1,52 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class TrueOrFalse extends Question {
+
+    private boolean correctAnswer;
+
+    public TrueOrFalse(int pointValue, String text, boolean correctAnswer) {
+        super(pointValue, text);
+        setPointValue(1);
+        this.correctAnswer = correctAnswer;
+    }
+
+    public boolean isCorrectAnswer (boolean possibleAnswer) {
+        if (possibleAnswer == correctAnswer) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public void displayAnswers() {
+        System.out.println("True or False");
+    }
+
+    @Override
+    public int getAnswers() {
+        Scanner answer = new Scanner(System.in);
+        System.out.println("What will your answer be? Type 't' for True and 'f' for False");
+        String userAnswer = answer.nextLine();
+        if (userAnswer.indexOf('t') >= 0) {
+            if (isCorrectAnswer(true)) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+        else {
+            if (isCorrectAnswer(false)) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+
+}
